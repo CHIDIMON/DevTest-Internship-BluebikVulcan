72c1bf8e1fd68e037c3e0e156292a02639740f01
CHIDIMON <chidimon@CHIDIMONs-MacBook-Air.local>
2025-12-23 20:01:59 +0700

    Update API Add filter and sort and tested endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,10 @@
 #main.py
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Depends, Query
+from fastapi import FastAPI, Depends, Query, HTTPException
 from database import create_db, get_session
 
-from typing import List
-from sqlmodel import Session, select
+from typing import List , Optional
+from sqlmodel import Session, select, col
 from schemas import Transaction
 
 @asynccontextmanager
@@ -21,9 +21,40 @@ def read_root():
 @app.get("/transactions", response_model=List[Transaction])
 def get_transactions(
     session: Session = Depends(get_session),
-    skip: int = 0, 
-    limit: int = Query(default=100, le=100) # Default 100, Max 100 items
+    page: int = Query(1, ge=1, description="Page number (starts at 1)"),
+    limit: int = Query(default=10, ge=1, le=100, description="Items per page (10-100)"),
+    search: Optional[str] = Query(None, description="Search by customer name"),
+    status: Optional[str] = Query(None, description="Filter by status (Completed, Pending, Failed)"),
+    sort_by: str = Query("date", description="Sort by field (date, amount)"),
+    order: str = Query("desc", description="Sort order (asc, desc)")
 ):
-    statement = select(Transaction).offset(skip).limit(limit)
+    statement = select(Transaction)
+
+    if search:
+        statement = statement.where(col(Transaction.customer_name).contains(search))
+    
+    #Filter Logic
+    if status and status.lower() != "all":
+        statement = statement.where(Transaction.status == status)
+
+    #Sorting Logic
+    if hasattr(Transaction, sort_by):
+        sort_column = getattr(Transaction, sort_by)
+        if order.lower() == "asc":
+            statement = statement.order_by(sort_column.asc())
+        else:
+            statement = statement.order_by(sort_column.desc())
+    
+    #Pagination Logic (Convert Page To Offset)
+    offset = (page - 1) * limit
+    statement = statement.offset(offset).limit(limit)
+
     results = session.exec(statement).all()
     return results
+
+@app.get("/transactions/{transaction_id}", response_model=Transaction)
+def get_transaction_detail(transaction_id: str, session: Session = Depends(get_session)):
+    item = session.get(Transaction, transaction_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+    return item
